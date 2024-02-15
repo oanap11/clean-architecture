2e376435be8ab29e51457a2f95d0bf2ef1711d61
oana <oana.apvel@gmail.com>
2024-02-15 23:12:34 +0200

    Add global error handling

--- DoubleDinner.Api/Program.cs
+++ DoubleDinner.Api/Program.cs
@@ -1,5 +1,7 @@
+using DoubleDinner.Api.Common.Errors;
 using DoubleDinner.Application;
 using DoubleDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -8,10 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
         .AddInfrastructure(builder.Configuration);
         
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, DoubleDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
